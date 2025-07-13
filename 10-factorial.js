1c7e13bc2501e3ed1b6a54c2e15142ce74fbc1d3
Daniella <apejidaniella@gmail.com>
2025-07-13 20:05:05 +0100

    add file 10

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(n) {
+    if (n <= 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+  
+  const num = Number.parseInt(process.argv[2]);
+  
+  console.log(factorial(Number.isNaN(num) ? 1 : num));
+  
